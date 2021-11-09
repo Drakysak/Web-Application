af3bf3742ed7020d242562ba198b3663994a2b76
Ivo Matoušek <Ivo.Mato@seznam.cz>
2021-11-09 22:10:15 +0100

    insert querry

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ app.post("/", async (req, res) => {
                 }else{
                         await client.query("INSERT INTO usersdata VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)", [
                                 req.body.Jmeno,
-                                reg.body.Prijmeni,
+                                req.body.Prijmeni,
                                 req.body.Email,
                                 req.body.Mechanik_elektrtechnik,
                                 req.body.Elektrikar,
