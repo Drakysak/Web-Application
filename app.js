ec2481718cb0681c73d7efb832cdaf309b2ee687
Ivo Matoušek <Ivo.Mato@seznam.cz>
2021-11-11 19:55:57 +0100

    table-display

--- app.js
+++ app.js
@@ -78,9 +78,8 @@ app.post("/", async (req, res) => {
 });
 
 app.get("/database", async (req,res) => {
+        const client = await pool.connect();
         try{
-                const client = await pool.connect();
-
                 const dataQuery = await client.query("SELECT * FROM usersdata");
 
                 const table = xlsx.utils.sheet_to_html(dataQuery.rows);
