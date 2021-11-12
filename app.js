e0a780441240852b3c28206f59b8b7f9f26a9bf8
Ivo Matoušek <Ivo.Mato@seznam.cz>
2021-11-12 20:46:25 +0100

    data

--- app.js
+++ app.js
@@ -97,11 +97,11 @@ app.get("/database", async (req,res) => {
                 console.log(email);
 
                 for(var i = 0; i < dataQuery.rows.length; i++){
-                        var condition = email.includes(JSON.stringify(emailQuery.rows[i]));
+                        var condition = email.includes(emailQuery.rows[i]);
 
                         console.log(condition);
 
-                        if(condition){
+                        if(email == emailQuery.rows[i]){
                                 console.log("v souboru se už nachází");
                         }else{
                                 data.push(dataQuery.rows[i]);                                
