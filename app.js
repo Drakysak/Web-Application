a3c19596a035cc4392ed9be6483b2d274f2018eb
Ivo Matoušek <Ivo.Mato@seznam.cz>
2021-11-17 19:42:22 +0100

    questions

--- app.js
+++ app.js
@@ -134,7 +134,7 @@ app.get("/questions/:id", (req, res) =>{
 app.post("/questions/:id", async(req, res) =>{
         const client = await pool.connect();
         try{
-                console.log(req.params.id)
+                console.log(req.params["id"])
                 if(req.body.email == "" ){
                         console.log("zdej email")
                 }
