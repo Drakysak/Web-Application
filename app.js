bd0850acfd99c01f456c9f105f9b4be81eb5e386
Ivo Matoušek <Ivo.Mato@seznam.cz>
2021-11-21 20:42:44 +0100

    errorMessages

--- app.js
+++ app.js
@@ -187,7 +187,7 @@ app.post("/questions/:id", async(req, res) =>{
 
                                 console.log(condition);
         
-                                if (condition == null){
+                                if (!condition){
                                         client.query("UPDATE userquestions SET q3 =$1 WHERE email=$2", [req.body.odpoved, req.body.email]);
                                 }else{
                                         req.flash('message', 'Už jse odpovídali!');
