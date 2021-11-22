6f0ea4bd4c0d8396cff0e64f09ecd0396471c3e6
Ivo Matoušek <Ivo.Mato@seznam.cz>
2021-11-22 11:16:06 +0100

    errormessages

--- app.js
+++ app.js
@@ -169,9 +169,17 @@ app.post("/questions/:id", async(req, res) =>{
 
                 if(condition){
                         if(req.params.id == "q1"){
-                                
+                                const conditions = client.query("SELECT q1 FROM userQuestions WHERE email = $1", [req.body.email]);
 
-                                client.query("UPDATE userquestions SET q1 =$1 WHERE email=$2", [req.body.odpoved, req.body.email]);
+                                console.log(conditions);
+                                
+                                if(conditions == null){
+
+                                        client.query("UPDATE userquestions SET q1 =$1 WHERE email=$2", [req.body.odpoved, req.body.email]);
+                                }else{
+                                        console.log("error")
+                                }
+
                                 
                         }else if(req.params.id == "q2"){
 
